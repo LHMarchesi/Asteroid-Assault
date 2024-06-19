00c550677f4678857acbdc82d0735f6f04884394
Fianriel <42074035+esteban18498@users.noreply.github.com>
2024-06-19 17:51:23 -0300

    Asteroids

--- FastAsteroid.cs
+++ FastAsteroid.cs
@@ -1,41 +1,22 @@
-﻿using MyGame.assets;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyGame
 {
-    public class Asteroid : GameObject
+    internal class FastAsteroid : Asteroid
     {
-        public event Action<Asteroid> Destroy;
         private Animation idleAnimation;
-        public static bool IsFast;
         private ObjectsMovement objectsMovement;
 
-
-        public Asteroid(Vector2 position, int speed) : base(position) // Construcrtor
+        public FastAsteroid(Vector2 position, int speed) : base(position, speed) // Construcrtor
         {
             CreateAnimations();
             transform = new Transform(position, new Vector2(75, 75));
             objectsMovement = new ObjectsMovement(transform, speed);
             Destroy += RemoveAsteroid;
-        }
-
-
-        public override void Update()
-        {
-            base.Update();
-            currentAnimation.Update();
-            objectsMovement.MoveDown();
-
-            if (transform.Position.y >= 1000)
-            {
-                transform.SetPosition(ObjectsMovement.SetRandomPosition());
-            }
         }
 
         private void CreateAnimations()
@@ -44,7 +25,7 @@ namespace MyGame
             List<IntPtr> idleTextures = new List<IntPtr>();
             for (int i = 0; i < 4; i++)
             {
-                IntPtr frame = Engine.LoadImage($"assets/Asteroid/{i}.png");
+                IntPtr frame = Engine.LoadImage($"assets/FastAsteroid/{i}.png");
                 idleTextures.Add(frame);
             }
             idleAnimation = new Animation("Idle", idleTextures, 20f, true);
@@ -52,19 +33,9 @@ namespace MyGame
 
         }
 
-        public void OnDestroy()
-        {
-            Destroy.Invoke(this);
-        }
-
         private void RemoveAsteroid(Asteroid asteroid)
         {
-
             GameManager.Instance.LevelManager.GameObjects.Remove(asteroid);
         }
-
-
     }
-
-
 }
